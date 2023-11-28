b29460fb6680313bc82eb45fef46cc3a376a3939
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-11-28 22:42:15 +0900

    SELECT MODE 화살표 추가

--- select_mode.py
+++ select_mode.py
@@ -1,13 +1,21 @@
+from pico2d import *
+
 import game_framework
 import play_mode
 import title_mode
-from pico2d import *
+
+game_mode = {'vs_CPU': 470, 'vs_Player': 358}
 
 
 def init():
     global image
+    global arrow
+    global arrow_y
 
     image = load_image('resource\\selection.png')
+    arrow = load_image('resource\\selection_arrow.png')
+
+    arrow_y = game_mode['vs_CPU']
 
 
 def finish():
@@ -21,15 +29,22 @@ def update():
 def draw():
     clear_canvas()
     image.draw(250 * 2, 225 * 2, 250 * 4, 225 * 4)
+    arrow.draw(75, arrow_y, 50, 50)
     update_canvas()
 
 
 def handle_events():
+    global arrow_y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN) and arrow_y == game_mode['vs_CPU']:
+            arrow_y = game_mode['vs_Player']
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP) and arrow_y == game_mode['vs_Player']:
+            arrow_y = game_mode['vs_CPU']
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE) and arrow_y == game_mode['vs_Player']:
+            game_framework.change_mode(play_mode)
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.change_mode(title_mode)
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-            game_framework.change_mode(play_mode)
